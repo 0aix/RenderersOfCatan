f96b24e2c7ac2785b18a8fa7f9839cc792d84909
Zain Humayun <zhumayun@yahoo-inc.com>
2017-08-04 15:38:05 -0700

    Added logic to Redner empty hexes correctly

--- include/textrenderer.h
+++ include/textrenderer.h
@@ -11,39 +11,55 @@ using namespace Catan::Generate;
 namespace Catan {
 	namespace Draw {
 		const int TEXT_HEX_WIDTH = 10;
-		const int TEXT_HEX_HEIGHT = 6;
+		const int TEXT_HEX_HEIGHT = 5;
 
 		void PopulateLineString(char **buffer, int rowIndex, int colIndex, string str) {
-			for (int i = 0; str.size(); i++) {
+    	for (int i = 0; i < str.length(); i++) {
 				char c = str.at(i);
 				buffer[rowIndex][colIndex++] = c;
 			}
 		}
 
 		void PopulateWithHex(char **buffer, int rowIndex, int colIndex, BoardNode *node) {
-			PopulateLineString(buffer, rowIndex, colIndex, "   _____  ");
+			PopulateLineString(buffer, rowIndex++, colIndex, "   _____  ");
+      PopulateLineString(buffer, rowIndex++, colIndex, "  /     \\ ");
+      PopulateLineString(buffer, rowIndex++, colIndex, " /       \\");
+      PopulateLineString(buffer, rowIndex++, colIndex, " \\       /");
+      PopulateLineString(buffer, rowIndex++, colIndex, "  \\_____/ ");
 		}
 
 		void RenderAsText(BoardGraph &graph) {
 			// Get Height and width requirements
 			const int BUFFER_HEIGHT = TEXT_HEX_HEIGHT * graph.BoardHeight();
-			const int BUFFER_WIDTH = TEXT_HEX_WIDTH * graph.BoardHeight();
+			const int BUFFER_WIDTH = TEXT_HEX_WIDTH * graph.BoardWidth();
 
 			char **buffer = new char*[BUFFER_HEIGHT];
 			for (int i = 0; i < BUFFER_HEIGHT; i++) {
 				buffer[i] = new char[BUFFER_WIDTH];
+        for (int j = 0; j < BUFFER_WIDTH; j++) {
+          buffer[i][j] = ' ';
+        }
 			}
 
 			BoardGraphForwardIterator it = graph.ForwardIterator();
 
+      // Add to buffer
 			for (int col = 0; col < BUFFER_WIDTH; col += TEXT_HEX_WIDTH) {
-				int rowLower = (BUFFER_HEIGHT - graph.ColumnHeight(col) * TEXT_HEX_HEIGHT) / 2;
-				int rowUpper = rowLower + graph.ColumnHeight(col) * TEXT_HEX_HEIGHT;
-				
+				int rowLower = (BUFFER_HEIGHT - graph.ColumnHeight(col / TEXT_HEX_WIDTH) * TEXT_HEX_HEIGHT) / 2;
+				int rowUpper = rowLower + graph.ColumnHeight(col / TEXT_HEX_WIDTH) * TEXT_HEX_HEIGHT;
+
 				for (; rowLower < rowUpper; rowLower += TEXT_HEX_HEIGHT) {
 					PopulateWithHex(buffer, rowLower, col, it.Next());
 				}
 			}
+
+      // Print the buffer
+      for (int row = 0; row < BUFFER_HEIGHT; row++) {
+        for (int col = 0; col < BUFFER_WIDTH; col++) {
+          cout << buffer[row][col];
+        }
+        cout << endl;
+      }
 
 			for (int i = 0; i < BUFFER_HEIGHT; i++) {
 				delete [] buffer[i];
